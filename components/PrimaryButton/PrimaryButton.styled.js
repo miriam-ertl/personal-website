0790d0246f46317e01780c15916774f613ae98f3
MErtl <m.ertl@steinberg.de>
2024-01-29 15:01:39 +0100

    feat: install prettier for typescript and javascript

--- components/PrimaryButton/PrimaryButton.styled.js
+++ components/PrimaryButton/PrimaryButton.styled.js
@@ -1,23 +1,23 @@
 import styled from "styled-components";
 
 export const PrimaryButton = styled.button`
-    display: inline-flex;
-    padding: 0.5rem 0.75rem;
-    justify-content: center;
-    align-items: center;
-    gap: 0.625rem;
-    background: var(--me-color-primary-color-gray-700);
-    color: var(--me-text-primary-text-inverted);
-    text-align: center;
-    font-size: 0.75rem;
-    cursor: pointer;
+  display: inline-flex;
+  padding: 0.5rem 0.75rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  background: var(--me-color-primary-color-gray-700);
+  color: var(--me-text-primary-text-inverted);
+  text-align: center;
+  font-size: 0.75rem;
+  cursor: pointer;
 
-    &:focus {
-        border: 2px solid var(--me-color-primary-color-gray-200);
-        background: var(--me-color-primary-color-gray-500);
-     }
+  &:focus {
+    border: 2px solid var(--me-color-primary-color-gray-200);
+    background: var(--me-color-primary-color-gray-500);
+  }
 
-     &:active {
-        background: var(--me-color-primary-color-gray-400);
-     }
+  &:active {
+    background: var(--me-color-primary-color-gray-400);
+  }
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,25 @@
-import { PrimaryButton } from "@/components/PrimaryButton/PrimaryButton.styled.js"
+import { PrimaryButton } from "@/components/PrimaryButton/PrimaryButton.styled.js";
 import { SecondaryButton } from "@/components/SecondaryButton/SecondaryButton.styled.js";
 
 export default function HomePage() {
-    return (
-    <main><h1>Wo Ideen ihre Reise machen – in der Welt der Codes.</h1>
-    <h2>Wo Ideen ihre Reise machen – in der Welt der Codes.</h2>
-    <h3>Wo Ideen ihre Reise machen – in der Welt der Codes.</h3>
-    <h4>Wo Ideen ihre Reise machen – in der Welt der Codes.</h4>
-    <h5>Wo Ideen ihre Reise machen – in der Welt der Codes.</h5>
-    <section><div>Wo Ideen ihre Reise machen – in der Welt der Codes.</div>
-    <div className="miscellaneous">Wo Ideen ihre Reise machen – in der Welt der Codes.</div>
-    <div className="copyright">Wo Ideen ihre Reise machen – in der Welt der Codes.</div></section>
-    <PrimaryButton>About</PrimaryButton>
-    <SecondaryButton>Work</SecondaryButton>
+  return (
+    <main>
+      <h1>Wo Ideen ihre Reise machen – in der Welt der Codes.</h1>
+      <h2>Wo Ideen ihre Reise machen – in der Welt der Codes.</h2>
+      <h3>Wo Ideen ihre Reise machen – in der Welt der Codes.</h3>
+      <h4>Wo Ideen ihre Reise machen – in der Welt der Codes.</h4>
+      <h5>Wo Ideen ihre Reise machen – in der Welt der Codes.</h5>
+      <section>
+        <div>Wo Ideen ihre Reise machen – in der Welt der Codes.</div>
+        <div className="miscellaneous">
+          Wo Ideen ihre Reise machen – in der Welt der Codes.
+        </div>
+        <div className="copyright">
+          Wo Ideen ihre Reise machen – in der Welt der Codes.
+        </div>
+      </section>
+      <PrimaryButton>About</PrimaryButton>
+      <SecondaryButton>Work</SecondaryButton>
     </main>
- ) }
-
-
+  );
+}
