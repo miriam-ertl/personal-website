20ac7a4ff6bdbb5942c189de8a9117f8f89b0e5a
MErtl <m.ertl@steinberg.de>
2024-01-29 16:43:10 +0100

    refactor: rename the colors for the secondary buttons

--- components/SecondaryButton/SecondaryButton.styled.js
+++ components/SecondaryButton/SecondaryButton.styled.js
@@ -1,26 +1,25 @@
 import styled from "styled-components";
 
 export const SecondaryButton = styled.button`
-    display: inline-flex;
-    padding: 0.5rem 0.75rem;
-    justify-content: center;
-    align-items: center;
-    gap: 0.625rem;
-    border: 1px solid var(--me-color-primary-color-gray-700);
-    background: var(--me-color-primary-color-white);
-    color: var(--me-text-primary-text-gray-700);
-    text-align: center;
-    font-size: 0.75rem;
+  display: inline-flex;
+  padding: 0.5rem 0.75rem;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  border: 1px solid var(--me-color-gray-700);
+  background: var(--me-color-white);
+  color: var(--me-text-gray-700);
+  text-align: center;
+  font-size: 0.75rem;
 
-     &:focus {
-        color: var(--me-text-primary-text-inverted);
-        border: 2px solid var(--me-color-primary-color-gray-200);
-        background: var(--me-color-primary-color-gray-500);
-     }
+  &:focus {
+    color: var(--me-text-inverted);
+    border: 2px solid var(--me-color-gray-200);
+    background: var(--me-color-gray-500);
+  }
 
-     &:active {
-        color: var(--me-text-primary-text-inverted);
-        background: var(--me-color-primary-color-gray-400);
-     }
-     
+  &:active {
+    color: var(--me-text-inverted);
+    background: var(--me-color-gray-400);
+  }
 `;
