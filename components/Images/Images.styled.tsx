925676834030abefd86444940537933806e86f3e
MErtl <m.ertl@steinberg.de>
2024-01-31 16:58:36 +0100

    feat: add tsyled component for images

---
+++ components/Images/Images.styled.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+export const CroppedCircleImage = styled(Image)`
+  position: relative;
+  overflow: hidden;
+  border-radius: 50%;
+`;
+
+export const RectangleImage = styled(Image)`
+  display: inlne;
+  height: 100%;
+  width: auto;
+  margin: 0 auto;
+`;
