ee945aca0e1560a1472b0fba70d59025e986524b
MErtl <m.ertl@steinberg.de>
2024-01-31 14:21:57 +0100

    feat: add Layout component with Container variable

---
+++ components/Layout/Layout.styled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
+`;
+
+export const Content = styled.div`
+  padding: 20px;
+`;
--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,16 +1,14 @@
-import {
-  Content,
-  Footer,
-  GridContainer,
-  Header,
-} from "@/components/Layout/Layout.styled.js";
+import { Container } from "@/components/Layout/Layout.styled";
+interface LayoutProps {
+  children: React.ReactNode;
+}
 
-export default function Layout({ children }) {
+export default function Layout({ children }: LayoutProps) {
   return (
-    <GridContainer>
+    <Container>
       <Header />
       <Content>{children}</Content>
       <Footer />
-    </GridContainer>
+    </Container>
   );
 }
