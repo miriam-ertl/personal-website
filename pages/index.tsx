445fb596bc6e2b8e5e9da836d2d3514c7adde35e
MErtl <m.ertl@steinberg.de>
2024-01-31 15:52:26 +0100

    refactor: adjust HTML structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import { PrimaryButton } from "@/components/PrimaryButton/PrimaryButton.styled";
 
 export default function HomePage() {
   return (
-    <div>
+    <main>
       <h1>Header</h1>
       <h2>Content</h2>
       <h3>Footer</h3>
       <PrimaryButton>hello</PrimaryButton>
-    </div>
+    </main>
   );
 }
