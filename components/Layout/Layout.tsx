bdf61c81112b4c3a25f8a55770bc4fd725508d37
MErtl <m.ertl@steinberg.de>
2024-01-31 15:17:00 +0100

    feat: adjust imports in Layout component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,4 +1,6 @@
 import { Container } from "@/components/Layout/Layout.styled";
+import { Content } from "@/components/Layout/Layout.styled";
+import { Footer } from "@/components/Footer/Footer.styled";
 import { Header } from "@/components/Header/Header.styled";
 
 interface LayoutProps {
