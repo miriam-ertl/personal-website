2e1e4f1524c7c1dd71b9a35f2245621d12c4e204
MErtl <m.ertl@steinberg.de>
2024-01-31 15:18:50 +0100

    feat: adjust content elements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import { PrimaryButton } from "@/components/PrimaryButton/PrimaryButton.styled";
+
 export default function HomePage() {
   return (
-    <div className="container">
-      <header className="header">logo</header>
-      <main className="content">für sections</main>
-      <footer className="footer">social</footer>
+    <div>
+      <h1>Header</h1>
+      <h2>Content</h2>
+      <h3>Footer</h3>
+      <PrimaryButton>hello</PrimaryButton>
     </div>
   );
 }
