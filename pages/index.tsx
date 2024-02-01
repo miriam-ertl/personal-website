7aef1533de95721ec07c1b32a41acf0d23600982
MErtl <m.ertl@steinberg.de>
2024-02-01 14:52:11 +0100

    refactor: Adjust content with image properties

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import Image from "@/components/Image/Image";
+import Image, { StyledImageProps } from "@/components/Image/Image";
+
 import { PrimaryButton } from "@/components/PrimaryButton/PrimaryButton.styled";
 import { SecondaryButton } from "@/components/SecondaryButton/SecondaryButton.styled";
 
@@ -6,15 +7,15 @@ export default function HomePage() {
   return (
     <main>
       <section>
-        <h1>Wo Ideen ihre Reise machen – in der Welt der Codes.</h1>
+        <h1>{content.headline}</h1>
         <Image
-          src="/placeholder-300-310.jpg"
-          width={300}
-          height={310}
-          alt="placeholder"
-          variant="default"
+          src={content.image.src}
+          width={content.image.width}
+          height={content.image.height}
+          alt={content.image.alt}
+          variant={content.image.variant}
         />
-        <PrimaryButton>hello</PrimaryButton>
+        <PrimaryButton>{content.button}</PrimaryButton>
       </section>
       <section>
         <hgroup>
@@ -58,3 +59,30 @@ export default function HomePage() {
     </main>
   );
 }
+
+interface ContentProps {
+  headline: string;
+  image: StyledImageProps;
+  button: string;
+  variant: "bright" | "dark";
+}
+
+export const content: ContentProps = {
+  headline: "Wo Ideen ihre Reise machen – in der Welt der Codes.",
+  image: {
+    src: "/placeholder-300-310.jpg",
+    width: 300,
+    height: 310,
+    alt: "placeholder",
+    variant: "default",
+  },
+  button: "hello",
+  variant: "bright",
+};
+
+interface ContentProps2 {
+  headlinegroup: { headline: string; headlinesmall: string };
+  parapgraph: string;
+}
+
+export const content2: ContentProps2 = {};
