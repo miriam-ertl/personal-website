229a48d587ae1bbc1b1955dc6558385118211470
MErtl <m.ertl@steinberg.de>
2024-02-02 19:35:41 +0100

    refactor: Include styled component properties in layout component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,7 +1,16 @@
-import { Container } from "@/components/Layout/Layout.styled";
-import { Content } from "@/components/Layout/Layout.styled";
 import Footer from "@/components/Footer/Footer";
 import Header from "../Header/Header";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
+`;
+
+export const Content = styled.div`
+  padding: 20px;
+`;
 
 interface LayoutProps {
   children: React.ReactNode;
