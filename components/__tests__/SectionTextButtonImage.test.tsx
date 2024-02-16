3663384b0da8e00c0b5e16d2166784795cc224ec
MErtl <m.ertl@steinberg.de>
2024-02-16 16:24:07 +0100

    test: Add new test for section with text, button, image

---
+++ components/__tests__/SectionTextButtonImage.test.tsx
@@ -0,0 +1,27 @@
+import "@testing-library/jest-dom/jest-globals";
+import "@testing-library/jest-dom";
+
+import { render, screen } from "@testing-library/react";
+
+import SectionTextButtonImage from "../SectionTextButtonImage/SectionTextButtonImage";
+
+describe(SectionTextButtonImage.name, () => {
+  it("renders a heading", () => {
+    render(
+      <SectionTextButtonImage
+        headline={"hallo"}
+        button={"about"}
+        image={{
+          src: "/placeholder-1920.jpg",
+          width: 230,
+          height: 230,
+          alt: "placeholder",
+          variant: "default",
+        }}
+      />
+    );
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
