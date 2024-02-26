81c1149a1d3add765bb45fdf89653babc6e476ed
MErtl <m.ertl@steinberg.de>
2024-02-26 10:46:38 +0100

    test: Write test to ask for components within homepage

---
+++ pages/__tests__/index.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+
+import SectionTextButtonImage from "@/components/SectionTextButtonImage/SectionTextButtonImage";
+
+describe(SectionTextButtonImage.name, () => {
+  it("renders a heading, a button and an image", () => {
+    render(
+      <SectionTextButtonImage
+        headline={"hallo"}
+        button={"about"}
+        image={{
+          src: "/placeholder-1920.jpg",
+          width: 230,
+          height: 230,
+          alt: "placeholder",
+          variant: "default",
+        }}
+      />
+    );
+    const heading = screen.getByRole("heading", { level: 1 });
+    expect(heading).toBeInTheDocument();
+
+    const button = screen.getByText(/about/i);
+    expect(button).toBeInTheDocument();
+
+    const image = screen.getByAltText(/placeholder/i);
+    expect(image).toHaveAttribute("alt");
+  });
+});
