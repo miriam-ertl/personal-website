7c83eeeb499005bbfcc56ecf30f7884cc49c970d
MErtl <m.ertl@steinberg.de>
2024-02-26 10:48:26 +0100

    test: Write test to ask for elements shown in component

--- components/__tests__/SectionTextButtonImage.test.tsx
+++ components/__tests__/SectionTextButtonImage.test.tsx
@@ -6,7 +6,7 @@ import { render, screen } from "@testing-library/react";
 import SectionTextButtonImage from "../SectionTextButtonImage/SectionTextButtonImage";
 
 describe(SectionTextButtonImage.name, () => {
-  it("renders a heading, a button and an image", () => {
+  it("renders a heading, a button and an image on bright background", () => {
     render(
       <SectionTextButtonImage
         headline={"hallo"}
@@ -18,6 +18,31 @@ describe(SectionTextButtonImage.name, () => {
           alt: "placeholder",
           variant: "default",
         }}
+        variant={"bright"}
+      />
+    );
+    const heading = screen.getByRole("heading", { level: 1 });
+    expect(heading).toBeInTheDocument();
+
+    const button = screen.getByRole("button", { name: /About/i });
+    expect(button).toBeInTheDocument();
+
+    const image = screen.getByAltText(/placeholder/i);
+    expect(image).toHaveAttribute("alt");
+  });
+  it("renders a heading, a button and a cropped image on dark background", () => {
+    render(
+      <SectionTextButtonImage
+        headline={"hallo"}
+        button={"about"}
+        image={{
+          src: "/placeholder-1920.jpg",
+          width: 230,
+          height: 230,
+          alt: "placeholder",
+          variant: "cropped",
+        }}
+        variant={"dark"}
       />
     );
     const heading = screen.getByRole("heading", { level: 1 });
