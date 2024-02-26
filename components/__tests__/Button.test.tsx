04ddca7a3e6c874afb59a8ca6fbc837e35723a04
MErtl <m.ertl@steinberg.de>
2024-02-26 13:57:56 +0100

    test: Describe Button.name instead of Button

--- components/__tests__/Button.test.tsx
+++ components/__tests__/Button.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 
 import Button from "@/components/Button/Button";
 
-describe("Button", () => {
+describe(Button.name, () => {
   it("renders a button", () => {
     render(<Button />);
 
